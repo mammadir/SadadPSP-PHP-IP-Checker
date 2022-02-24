47ab409b48e6e9c9aa60544f20602b4befc2fa45
Mohammad Majidi <persianscript.ir@gmail.com>
2022-02-24 14:36:59 +0330

    create ip.php

---
+++ ip.php
@@ -0,0 +1,29 @@
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>SadadPSP IP Checker</title>
+    <style type="text/css">
+    body{
+        padding-top:50px;
+        font-family:Arial;
+        text-align:center;
+        color:#000;
+        background:#f9b700;
+        
+    }
+    p{font-family:tahoma;font-size:12px;}
+    h2{font-size:45px;}
+    </style>
+    </head>
+    
+    <body>
+        
+        <?php $ip = $_SERVER['SERVER_ADDR'];?>
+        <h1>Server IP</h1>
+        <h2><?php echo $ip;?></h2>
+        
+        <p>پرداخت الکترونیک سداد</p>
+    </body>
+
+
+</html>
